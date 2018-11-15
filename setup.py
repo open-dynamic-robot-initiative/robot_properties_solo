b56a4fc8ad3e90ec690b789ac3a6a8d8cb7f638f
Naveau <maximilien.naveau@gmail.com>
2018-11-15 14:32:04 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+package_info = generate_distutils_setup()
+package_info['packages'] = ['py_robot_properties_quadruped']
+package_info['package_dir'] = {'': 'src'}
+package_info['install_requires'] = []
+
+setup(**package_info)
